c0198e7b99cb5fabb3d305db0f28e6dc34b462e7
Zheng Yune <13390592073@163.com>
2021-11-03 17:13:39 +0800

    implement binary search with iteration and recursion

--- binary_search.py
+++ binary_search.py
@@ -1,33 +1,33 @@
-def binary_search(lst, item):
+def binary_search(list, item):
     low = 0
-    high = len(lst) - 1
+    high = len(list) - 1
 
     while low <= high:
         mid = int((low + high) / 2)
-        guess = lst[mid]
-        if guess == item:
+        current = list[mid]
+        if current == item:
             return mid
-        if guess > item:
+        if current > item:
             high = mid - 1  # high 指针
         else:
             low = mid + 1  # low 指针
     return None
 
 
-def binary_search_recursion(theList, left, right, target):
+def binary_search_recursion(list, left, right, target):
     if left > right:
         return None
 
     midIndex = int((left + right) / 2)
-    midValue = theList[midIndex]
+    midValue = list[midIndex]
 
     if midValue == target:
         return midIndex
 
     elif target > midValue:
-        return binary_search_recursion(theList, midIndex + 1, right, target)
+        return binary_search_recursion(list, midIndex + 1, right, target)
     else:
-        return binary_search_recursion(theList, left, midIndex - 1, target)
+        return binary_search_recursion(list, left, midIndex - 1, target)
 
 
 my_list = [1, 3, 5, 7, 9]
